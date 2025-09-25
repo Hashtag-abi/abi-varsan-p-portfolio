9aaeb1e76fdac7e5b8ffb962e0851da97f99a1b2
Hashtag-abi <abivarsan20@gmail.com>
2025-09-25 12:28:53 +0530

    Add src/data/portfolioData.ts

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,61 @@
+export const portfolioData = {
+  "personalInfo": {
+    "name": "Abi varsan P",
+    "title": "DevOps",
+    "email": "Abi varsan P",
+    "phone": "Abi varsan P",
+    "location": "Abi varsan P",
+    "website": "Abi varsan P",
+    "bio": "Abi varsan P",
+    "profileImage": "",
+    "resumeFile": ""
+  },
+  "socialLinks": {
+    "linkedin": "Abi varsan P",
+    "github": "Abi varsan P",
+    "twitter": "Abi varsan P",
+    "instagram": "Abi varsan P"
+  },
+  "skills": [
+    {
+      "name": "Abi varsan P",
+      "level": 3,
+      "category": "Technical"
+    }
+  ],
+  "projects": [
+    {
+      "id": "1758781047821",
+      "title": "Abi varsan P",
+      "description": "Abi varsan P",
+      "technologies": [
+        "Abi varsan P"
+      ],
+      "liveUrl": "Abi varsan P",
+      "githubUrl": "Abi varsan P",
+      "imageUrl": "Abi varsan P"
+    }
+  ],
+  "experience": [
+    {
+      "id": "1758781056589",
+      "company": "Abi varsan P",
+      "position": "Abi varsan P",
+      "startDate": "",
+      "endDate": "",
+      "current": false,
+      "description": "Abi varsan P"
+    }
+  ],
+  "education": [
+    {
+      "id": "1758781060156",
+      "institution": "Abi varsan P",
+      "degree": "Abi varsan P",
+      "field": "Abi varsan P",
+      "startDate": "",
+      "endDate": "",
+      "gpa": "Abi varsan P"
+    }
+  ]
+};
